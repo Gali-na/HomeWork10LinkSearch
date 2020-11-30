bfe9f154b771bc996ab556ab54a6bc4387072803
Tsivka Galina <galinatsivka@gmail.come>
2020-11-30 09:44:50 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       /* String str ="<p><a href=\"dog.html\">Собаки</a></p>";
+        System.out.println(str.contains("<a href"));
+       char []cg =str.toCharArray();
+        System.out.println(str.indexOf("="));
+*/
+
+
+    }
+}
