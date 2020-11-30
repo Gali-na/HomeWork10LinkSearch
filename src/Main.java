a4e6cd55ef0b5b275e5c2619b6b2d327a65239c4
Tsivka Galina <galinatsivka@gmail.come>
2020-11-30 11:59:19 +0200

    decision

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-       /* String str ="<p><a href=\"dog.html\">Собаки</a></p>";
-        System.out.println(str.contains("<a href"));
-       char []cg =str.toCharArray();
-        System.out.println(str.indexOf("="));
-*/
-
-
+        WriteUrlInFile.serchLinc("https://ru.wikipedia.org/wiki/Java");
     }
 }
